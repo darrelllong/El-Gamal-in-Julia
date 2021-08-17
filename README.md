6f13966ebe89238448b3c3aee2370dd411ce2392
Darrell Long <darrell@dmz.local>
2021-08-16 21:18:52 -0700

    Initial reivison

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# El-Gamal-in-Julia
-A simple implementation of the El Gamal encryption system in the Julia language
+# El Gamal in Julia
+A simple implementation of the El Gamal encryption system in the Julia language.
+
+Originally written for the students at the University of California, Santa Cruz.
+
+```
+@article{elgamal1985public,
+  title={A public key cryptosystem and a signature scheme based on discrete logarithms},
+  author={ElGamal, Taher},
+  journal={IEEE transactions on information theory},
+  volume={31},
+  number={4},
+  pages={469--472},
+  year={1985},
+  publisher={IEEE}
+}
+```
+
+The implementation uses *safe primes* for creating the keys. This can be a little
+slow, but you do not need to do this more than once.
+
+```
+@misc{cryptoeprint:2001:007,
+    author       = {Ronald Rivest and Robert Silverman},
+    title        = {Are 'Strong' Primes Needed for {RSA}?},
+    howpublished = {Cryptology ePrint Archive, Report 2001/007},
+    year         = {2001},
+    note         = {\url{https://ia.cr/2001/007}},
+}
+
+```
+# Usage
+
+```
+(prv, pub) = keys(bits)
+
+c = encrypt(encode("string"), pub)
+
+m = decode(decrypt(c, prv)
+```
+
+Running it on the command line executes interactively to encrypt and decrypt strings.
+
+```
+dmz :: ~/El-Gamal-in-Julia » ./elgamal.jl
+How many bits? 128
+pub = (572927111265326666004563340005006575427, 65537, 106689413353486727133802126941609549788)
+prv = (572927111265326666004563340005006575427, 572881428683612937908443576094045369689)
+>> Hi Buckaroos!
+En[Hi Buckaroos!] = (319834251681942755631187036386350456050, 516191262128696593557688409582791082418)
+De[(319834251681942755631187036386350456050, 516191262128696593557688409582791082418)] = Hi Buckaroos!
+>> ^D
+```
---
+++ elgamal.jl
@@ -0,0 +1,170 @@
+#!/usr/bin/env julia
+
+using Random
+
+#=
+a^b (mod n) using the method of repeated squares.
+
+The key here is that every integer can be written as a sum of powers of 2 (binary numbers)
+and that includes the exponent. By repeated squaring we get a raised to a power of 2. Also
+recall that a^b * a^c = a^(b + c), so rather than adding we multiply since we are dealing
+with the exponent.
+=#
+
+function powerMod(a, d, n)
+    v = 1 # Value
+    p = a # Powers of a
+    while d > 0
+        if isodd(d) # 1 bit in the exponent
+           v = (v * p) % n
+        end
+        p = p^2 % n # Next power of two
+        d >>>= 1
+    end
+    v
+end
+
+#=
+Greatest common divisor, Euclidean version.
+=#
+
+function gcd(a, b)
+    while b ≠ 0
+        a, b = b, a % b
+    end
+    a
+end
+
+#=
+Witness loop of the Miller-Rabin probabilistic primality test.
+=#
+
+function witness(a, n)
+    u, t = n - 1, 0
+    while iseven(u) # n = u * 2^t + 1
+        t += 1   # Increase exponent
+        u >>>= 1 # Decrease the multiplier
+    end
+    x = powerMod(a, u, n)
+    for i in 1:t
+        y = powerMod(x, 2, n)
+        if y == 1 && x ≠ 1 && x ≠ n - 1
+            return true
+        end
+        x = y
+    end
+    x ≠ 1
+end
+
+function isPrime(n, k)
+    if n < 2 || (n ≠ 2 && iseven(n)) # 0, 1, and even except for 2 are not prime.
+        return false
+    elseif n < 4 # 3 is prime
+        return true
+    end # We must test all others
+    for j in 1:k
+        a = rand(2:n - 2) # Choose a random witness
+        if witness(a, n)
+            return false
+        end
+    end
+    true
+end
+
+#=
+We need a random prime number in [low, high] and for now a 1/4^100 chance of a composite is
+good enough.
+=#
+
+function randomPrime(low, high)
+    guess = 0 # Certainly not prime!
+    while !isPrime(guess, 100)
+        guess = rand(low:high) # Half will be even, the rest have Pr[prime] ≈ 1/log(N).
+    end
+    guess
+end
+
+#=
+A safe prime is the one following a Sophie German prime. If prime(p) and prime(2p + 1) then
+2p + 1 is a safe prime.
+=#
+
+function safePrime(low, high)
+    p = randomPrime(low, high)
+    while !isPrime(2 * p + 1,100)
+        p = randomPrime(low, high)
+    end
+    return 2 * p + 1
+end
+
+function generator(n, p)
+    g = n
+    q = (p - 1) ÷ 2
+    while powerMod(g, 2, p) == 1 && powerMod(g, q, p) == 1
+        g = g + 1
+    end
+    g
+end
+
+function keys(k)
+    p = safePrime(big"2"^(k - 1), big"2"^k - 1)
+    r = generator(big"2"^16 + 1, p)
+    a = rand((p - 1) ÷ 2:p - 1)
+    b = powerMod(r, a, p)
+    ((p, a), (p, r, b))
+end
+
+function encrypt(m, key)
+    (p, r, b) = key
+    k = rand(1:p - 2)
+    𝛾 = powerMod(r, k, p)
+    𝛿 = (m * powerMod(b, k, p)) % p
+    (𝛾, 𝛿)
+end
+
+function decrypt(m, key)
+    (p, a) = key
+    (𝛾, 𝛿) = m
+    (powerMod(𝛾, p - 1 - a, p) * 𝛿) % p
+end
+
+function encode(s)
+    sum::BigInt = 0
+    pow::BigInt = 1
+    for c in s
+        sum += pow * (0xAA ⊻ BigInt(c))
+        pow *= 256
+    end
+    sum
+end
+
+#=
+Transform a BigInt back into a string, subtracting off the 0xAA. We treat it as a base-256
+integer and just pull off the digits.
+=#
+
+function decode(n)
+    s = ""
+    while n > 0
+        s = s * Char(0xAA ⊻ (n % 256))
+        n ÷= 256
+    end
+    s
+end
+
+print("How many bits? ")
+
+bits = parse(Int64, readline())
+
+(prv, pub) = keys(bits)
+
+println("pub = $pub")
+println("prv = $prv")
+
+print(">> ")
+for m in eachline()
+    c = encrypt(encode(m), pub); println("En[$m] = $c")
+    t = decode(decrypt(c, prv)); println("De[$c] = $t")
+    print(">> ")
+end
+
